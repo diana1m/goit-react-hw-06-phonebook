919b27da85fa55c66cb946108f9223fd8552e508
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2023-04-06 13:21:23 +0300

    add redux persist

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import { App } from 'components/App';
+import { Loader } from 'components/Loader/Loader';
+
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { store, persistor } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={<Loader/>} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+ 
+const persistConfig = {
+  key: 'contacts',
+  storage,
+  whitelist: ['contacts']
+}
+ 
+const persistedReducer = persistReducer(persistConfig, contactsReducer);
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistedReducer,
   },
 });
+
+export const persistor = persistStore(store);
